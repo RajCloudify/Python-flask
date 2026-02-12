27ae35baf08dc7713690497f1253f344c047b1c2
Raj Attingal <rajattingal1@gmail.com>
2026-02-12 13:12:17 +0400

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Flask Sample Application is Functional"
+    return "Flask Sample Application is Functional" 
 
 
 if __name__ == "__main__":
